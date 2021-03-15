10fa4390dcbd2e6ffafc9350cb2c7bb4de1f2499
rich11berr <rinocraft.tv@gmail.com>
2021-03-15 23:53:20 +0400

    feat: finis TS task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arrayTS =  [];
+    if (matrix === undefined || matrix.length === 0) {
+      return [];
+    }
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 === 0) {
+        let x = matrix[i].length;
+        for (j = 0; j < matrix[i].length; j++) {
+          arrayTS.push(matrix[i][j]);
+        } 
+      }
+      else {
+          for (j = matrix[i].length - 1; 0 <=j; j--) {
+            arrayTS.push(matrix[i][j]);
+          }
+        }
+    }
+    return arrayTS;
 }
